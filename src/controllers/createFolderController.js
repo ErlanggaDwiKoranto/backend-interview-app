005a88f3932a11930abdc8a782ce07cc2ccdc1e2
erlangga <erlanggadwikoratno@gmail.com>
2024-10-15 21:51:21 +0700

    fakjnfkadadadcC

--- src/controllers/createFolderController.js
+++ src/controllers/createFolderController.js
@@ -2,18 +2,50 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const { google } = require('googleapis');
-const path = require('path');
-const fs = require('fs');
-
-// Path to your Service Account credentials
-const credentialsPath = path.join(__dirname, '..', 'config', 'credentials.json');
-const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf8'));
 
 // Initialize the Google Drive API client
 const auth = new google.auth.GoogleAuth({
-    credentials,
+    credentials: {
+      type: "service_account",
+      project_id: "interviewtelkom",
+      private_key_id: "f16ffab6d6a724cd593e42aafe535f1c61302cec",
+      private_key: `-----BEGIN PRIVATE KEY-----
+  MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6ekVQ+zWw1Iup
+  m7XYA4tjPs4EXKbEL3K9X3YGooYBCSmePEYl9TdpYSQUeV7bYe2m7aUJkzoEFppR
+  0kSxVBnDwA6GfHLc531tnMNk4CV8XbD7Z8wmTRZ49KIoYuiA129Dx6tOj54OWnV8
+  JNwWX+6FSY8n/YH08j4scSyOsx6lYNg8fwvkUzg3MP93ZPulQOaaJSziDnwCQE8B
+  +c41ZFO7kL4hY2zwf3m2U1C00lJWvYLshMmyM7Ziak9wcins+qD9knhLU2CeC3ot
+  G/7L9sLOxDHBuQTIxLW3+PcLF85mNgkvjfob7P4Gv+ZNzfKBIX1NucYBYlrFpT3+
+  MmikdpW7AgMBAAECggEAOiR5jGBnQzViPoXwUuP4B//DGJUS4kSxD9693B+8jIIg
+  2deNP7SPjGnfaArXyqtkABJEzRF8m124zB41kNzaE7DEVqZi4gHDl/Oy8Lb6uSY1
+  Uo3wl1id0yTjkpgUSmfhp06QWTRqYatOymBrxwYuRpzC6AMhDBCj1hOLgwrSWVmo
+  /3jQ2+ubeWy1nIj68PBrQUlCuke35BBcTryu+cTCNlydxmHXCubUTR/L0w26GwQX
+  pMCBBYcoEo6clZV2rjBSe94rLOFofuVkBT+u4uYcKXOaFA61Ll1zbz9GWh9CH52f
+  CucQFsYoW9K93OSTrXEMViyiJ1e8ebwSDRIdrMERsQKBgQD+UcZK+yEjyhpyQrAY
+  ker/y+orLjxIzXOG88TrvLEp8+z7P+0J4b6BwU0RuG0IR/rpgj6o7DA5jx4Va09P
+  6m36+zAmtCCoTcujcxhXzHv2LAXsnKsbfK7v4RgokyzrKJWQNOF2xCv1t/b1mvrS
+  pc02s6Uso4xKFLSvph1mFK5SjwKBgQC7tbrhEu78la1NXzg9FCU2/UecGt86JkoI
+  fIrBkSIQAgkWTjyQc0f7Vx5WmnTPRcgAz9M/cSVRjojmDdR/T4J+BGFg6XNZEhx5
+  Duida082lEOADmsuKdmqUZ62jBe8PgGB3hvAIaEfIl8U1ZJ/RVVOTcwIMNYkhG7R
+  OuOwneEwFQKBgEUI8ajc7qwGapNlouOoiIATXGeIT3j7vEZ/2Y6cVAMtYcD6I2Ax
+  VUNns4IB7Cb5UxUb8+Jq6tRVEzeBP7BT1LLyiKmY79x/8a1Wyt7adTPn01vdU11M
+  xTf8hFBPUzwqch8JG8LfkYdL/s/A2CFiPIiCz22En8pkQIAFualCJ+flAoGAVXv/
+  rXzH5bPshq30mKxR/mb+c7MxMfQ6Bv9g2aJU1uF2bfgXUNmPUIFuSZZx1+s5mFUN
+  KXlHu4qkAdEOkyQMk4M/LbljVI8Q9Hi7quUx3/NJgkoOualJfW1KUGwgaxP578DM
+  9oTaMamfj4dr/hX/MXfwkVJcU6kUJqkhBFACO7kCgYEAgZPhW9czhKPA4DzZk0AR
+  mbYSRuhDxA44Da15ViLWm5REprcw/cFy6a/TZj3wAtJ/a2ZHv9ymIfsoIZtKLvHo
+  6QhfGinhldwQ9A0pqMbxfDSj9M3kTOmmyZW5tmLsYxCGx08YyUqiFskJCpkH7yom
+  9vhr+rTpRiOeTUHmOUEwOWU=`,
+      client_email: "telkom-interview@interviewtelkom.iam.gserviceaccount.com",
+      client_id: "107900815478722730809",
+      auth_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_uri: "https://oauth2.googleapis.com/token",
+      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+      client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/telkom-interview@interviewtelkom.iam.gserviceaccount.com"
+    },
     scopes: ['https://www.googleapis.com/auth/drive']
-});
+  });
+  
 
 const drive = google.drive({ version: 'v3', auth });
 
